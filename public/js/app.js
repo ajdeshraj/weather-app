204a06ed4665573369408ac8a403dd9c1e8891e4
Arjun Deshraj <arjun.deshraj@gmail.com>
2023-07-03 17:24:56 +0530

    Added client side scripting to fetch weather forecast information

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,30 @@
 console.log('Running Client Side JavaScript')
+
+const weatherForm = document.querySelector('form')
+const searchElement = document.querySelector('input')
+const messageOne = document.querySelector('#message-one')
+const messageTwo = document.querySelector('#message-two')
+
+weatherForm.addEventListener('submit', (e) => {
+    e.preventDefault()
+
+    const location = searchElement.value
+    console.log(location)
+    const url = 'http://localhost:3000/weather?address=' + location
+
+    // Clearing content of the two paragraph tags
+    messageOne.textContent = 'Loading... '
+    messageTwo.textContent = ''
+
+    fetch(url).then((response) => {
+        response.json().then((data) => {
+            if (data.error) {
+                messageOne.textContent = data.error
+            }
+            else {
+                messageOne.textContent = data.location
+                messageTwo.textContent = data.description + '. Temperature is ' + data.temperature + '. Feels like ' + data.feelsLike
+            }
+        })
+    })
+})
